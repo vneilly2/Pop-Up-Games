f16b058e456347a8ebf7755b52249c4a6dacee89
Max Berkowitz <maxl.berkowitz@gmail.com>
2018-07-18 16:53:57 -0700

    Added login and logout on server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const bodyParser = require('body-parser');
 const utils = require('./utils');
-const db = require('../db/index.js');
+const db = require('../database/helpers.js');
 const app = require('express')();
 app.use(require('express-session')({
   secret: process.env.SECRET || require('../config/config.js').SECRET,
@@ -15,117 +15,114 @@ app.use(express.static(__dirname + '../client/dist'));
 
 
 //1
-app.post('/signup', (req, res) => {
+app.post('/signup', (req, res) => util.postRes(() =>
+  //hash the password
+  util.hashPass(req.body.password)
+    //send the user data to the database
+    .then(pass => db.saveUser((req.body.password = pass) && req.body))
+//send an error message to the basic post response
+, res, 'username is taken'));
 
-});
-
-app.post('/login', (req, res) => {
-  req.session.regenerate(() => res.status(200).send(req.session.user = req.body.username));
-});
+app.post('/login', (req, res) =>
+  //request the hashed password from the database
+  db.getPass(req.body)
+    //check that the password and the hashed password are the same
+    .then(hash => util.checkPass(req.body.password, hash))
+    //either create the session or return a lack of a match
+    .then(matches => matches
+      ? req.session.regenerate(() => res.status(200).send(req.session.user = req.body.username))
+      : res.status(422).send(matches))
+    //if there was an issue, assume that the username was not found in the database
+    .catch(err => app.status(404).send('username not found'))
+);
 
 app.get('/logout', (req, res) => req.session.destroy(() => res.sendStatus(200)));
 
-app.post('/event/guest', util.checkUser, (req, res) => {
+app.post('/event/guest', util.checkUser, (req, res) => util.postRes(() =>
+  //add a quest to the event
+  db.saveGuest({
+    event: req.body.eventName,
+    guest: req.session.user
+  })
+, res));
 
-});
+app.post('/event', util.checkUser, (req, res) => util.postRes(() =>
+  db.saveEvent((req.body.username = req.session.user) && req.body)
+, res));
 
-app.post('/event', util.checkUser, (req, res) => {
+app.get('/event', util.checkUser, (req, res) => util.getRes(() =>
+  db.getEvent({
 
-});
+  })
+, res));
 
-app.get('/event', util.checkUser, (req, res) => {
+app.get('/events', util.checkUser, (req, res) => util.getRes(() =>
+  db.getEvents({
 
-});
+  })
+, res));
 
-app.get('/events', util.checkUser, (req, res) => {
+app.get('/field', util.checkUser, (req, res) => util.getRes(() =>
+  db.getField({
 
-});
+  })
+, res));
 
-app.get('/field', util.checkUser, (req, res) => {
+app.post('/field', util.checkUser, (req, res) => util.postRes(() =>
+  db.saveField({
 
-});
+  })
+, res));
 
-app.post('/field', util.checkUser, (req, res) => {
+app.get('/venue', util.checkUser, (req, res) => util.getRes(() =>
+  db.getVenue({
 
-});
+  })
+, res));
 
-app.get('/venue', util.checkUser, (req, res) => {
+app.post('/venue', util.checkUser, (req, res) => util.postRes(() =>
+  db.saveVenue({
 
-});
-
-app.post('/venue', util.checkUser, (req, res) => {
-
-});
+  })
+, res));
 
 
 
 //2
-app.delete('/event/guest', util.checkUser, (req, res) => {
+app.delete('/event/guest', util.checkUser, (req, res) => {});
 
-});
+app.get('/event/guest', util.checkUser, (req, res) => {});
 
-app.get('/event/guest', util.checkUser, (req, res) => {
+app.put('/event', util.checkUser, (req, res) => {});
 
-});
+app.put('/field', util.checkUser, (req, res) => {});
 
-app.put('/event', util.checkUser, (req, res) => {
+app.get('/user/events', util.checkUser, (req, res) => {});
 
-});
+app.get('/user/passreset', util.checkUser, (req, res) => {});
 
-app.put('/field', util.checkUser, (req, res) => {
+app.get('/user', util.checkUser, (req, res) => {});
 
-});
+app.put('/user', util.checkUser, (req, res) => {});
 
-app.get('/user/events', util.checkUser, (req, res) => {
+app.delete('/user', util.checkUser, (req, res) => {});
 
-});
+app.get('/sport', util.checkUser, (req, res) => {});
 
-app.get('/user/passreset', util.checkUser, (req, res) => {
+app.post('/sport', util.checkUser, (req, res) => {});
 
-});
+app.put('/sport', util.checkUser, (req, res) => {});
 
-app.get('/user', util.checkUser, (req, res) => {
-
-});
-
-app.put('/user', util.checkUser, (req, res) => {
-
-});
-
-app.delete('/user', util.checkUser, (req, res) => {
-
-});
-
-app.get('/sport', util.checkUser, (req, res) => {
-
-});
-
-app.post('/sport', util.checkUser, (req, res) => {
-
-});
-
-app.put('/sport', util.checkUser, (req, res) => {
-
-});
-
-app.delete('/sport', util.checkUser, (req, res) => {
-
-});
+app.delete('/sport', util.checkUser, (req, res) => {});
 
 
 
 //3
-app.put('/sportsmerge', util.checkUser, (req, res) => {
+app.put('/sportsmerge', util.checkUser, (req, res) => {});
 
-});
+app.put('/venuemerge', util.checkUser, (req, res) => {});
 
-app.put('/venuemerge', util.checkUser, (req, res) => {
-
-});
-
-app.put('/fieldmerge', util.checkUser, (req, res) => {
-
-});
+app.put('/fieldmerge', util.checkUser, (req, res) => {});
 
 
 
--- server/utils.js
+++ server/utils.js
@@ -1,16 +1,30 @@
 const bcrypt = require('bcrypt-nodejs');
 const axios = require('axios');
 
-exports.checkUser = (req, res, next) => req.session.user ? next() : res.status(301).send('login');
+//check to see if there is a session or not before continuing
+exports.checkUser = (req, res, next) => req.session.user ? next() : res.status(401).send('login');
 
+//check to see if the user is an admin before continuing
 exports.checkAdmin = (req, res, next) => {};
 
+//use bcrypt to hash a password
 exports.hashPass = pass =>
    new Promise((resolve, reject) =>
     bcrypt.hash(pass, null, null, (err, hash) =>
       err ? reject(err) : reolve(hash)));
 
+//use bcrypt to whether or not the password matches the hash, resolve with true or false
 exports.checkPass = (pass, hash) =>
   new Promise((resolve, reject) =>
     bcrypt.compare(pass, hash, (err, matches) =>
       err ? reject(err) : resolve(matches)));
+
+//a basic get resovle
+exports.getRes = (p, res, errMessage) => p()
+  .then(data => res.status(200).send(data))
+  .catch(err => res.status(400).send(errMessage || err));
+
+//a basic post resovle
+exports.postRes = (p, res, errMessage) => p()
+  .then(() => res.sendStatus(201))
+  .catch(err => res.status(400).send(errMessage || err));
