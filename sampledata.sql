a5a9713ea55c4667658b9c7ac82e34df7083275b
yuqi <zyqandrewda@gmail.com>
2018-07-20 16:20:55 -0400

    sampledata revised

--- sampledata.sql
+++ sampledata.sql
@@ -4,13 +4,13 @@ VALUES ("yuqizhu", "yuqi", "zhu", "admin", "100 5th ave", "123@qq.com");
 INSERT INTO events (name, startBlock, endBlock, Notes, minPlayer, maxPlayer, ownerId)
 VALUES ("badminton club", 30, 32, "This is a badminton event", 0, 4, 1);
 
-INSERT INTO venues ("name", "address")
+INSERT INTO venues (name, address)
 VALUES ("Brandon Gym", "200 5th Ave");
 
 INSERT INTO sports (name)
 VALUES ("badminton");
 
-INSERT INTO fields ("name", "Notes", "type")
+INSERT INTO fields (name, Notes, type)
 VALUES ("badminton racket", "Free", "It's a type.");
 
 INSERT INTO messages (body)
