4bc4d51bac09c7f6a568130a8c018d535902cd8c
Sophia Lee <soph915@gmail.com>
2018-07-25 12:11:05 -0700

    add basketball to dist img folder

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -52,10 +52,10 @@ class App extends React.Component {
 
 
             {/* First Grid */}
-            <header class="w3-container w3-blue w3-center" id="homeheader" style={this.state.loggedIn ? {display:'none'} : {} }>
-              <h1 class="w3-margin w3-jumbo">Pop-Up-Games</h1>
-              <p class="w3-xlarge">Play Games! Have Fun!</p>
-              <Link to="/signup" style={this.state.loggedIn ? {display:'none'} : {} }  class="w3-button w3-white w3-padding-large w3-large w3-margin-top">Sign Up</Link>
+            <header className="w3-container w3-blue w3-center" id="homeheader" style={this.state.loggedIn ? {display:'none'} : {} }>
+              <h1 className="w3-margin w3-jumbo">Pop-Up-Games</h1>
+              <p className="w3-xlarge">Play Games! Have Fun!</p>
+              <Link to="/signup" style={this.state.loggedIn ? {display:'none'} : {} }  className="w3-button w3-white w3-padding-large w3-large w3-margin-top">Sign Up</Link>
             </header>
             <Route path="/login"
               render={props => <Login
@@ -66,16 +66,16 @@ class App extends React.Component {
 
             {/* Second Grid */}
 
-            <div class="w3-row-padding w3-padding-64 w3-container" style={this.state.loggedIn ? {display:'none'} : {} }>
-              <div class="w3-content">
-                <div class="w3-twothird">
+            <div className="w3-row-padding w3-padding-64 w3-container" style={this.state.loggedIn ? {display:'none'} : {} }>
+              <div className="w3-content">
+                <div className="w3-twothird">
                   <h1>What We Do</h1>
-                  <h5 class="w3-padding-32 w3-text-grey" >We here love sports and we want make it fun and easy to find sport games in your local area. Find and join a game or go on and create your own. Go out and have fun!</h5>
+                  <h5 className="w3-padding-32 w3-text-grey" >We here love sports and we want make it fun and easy to find sport games in your local area. Find and join a game or go on and create your own. Go out and have fun!</h5>
 
                 </div>
 
-                <div class="w3-third w3-center">
-                  <img src='./bluebasketballicon.png' height="300" />
+                <div className="w3-third w3-center">
+                  <img src='./img/bluebasketballicon.png' height="300" />
                 </div>
               </div>
             </div>
@@ -98,6 +98,6 @@ ReactDOM.render(<App />, document.getElementById('app'));
 
 
 //moved logOut to navbar
-// <div class="w3-bar w3-blue w3-card w3-left-align w3-large">
-//   <Link to="/logout" style={this.state.loggedIn ? {} : {display:'none'} } className="w3-button w3-padding-large w3-white">Logout</Link>
+// <div className="w3-bar w3-blue w3-card w3-left-align w3-large">
+//   <Link to="/logout" style={this.state.loggedIn ? {} : {display:'none'} } classNameName="w3-button w3-padding-large w3-white">Logout</Link>
 // </div>
