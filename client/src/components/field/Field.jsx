8952ff066c82a8e7816de7a7fc7c3f8f73f83bd7
Sophia Lee <soph915@gmail.com>
2018-07-27 09:58:54 -0700

    update field component

--- client/src/components/field/Field.jsx
+++ client/src/components/field/Field.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import FieldCalendar from './FieldCalendar.jsx';
 import FieldInfo from './FieldInfo.jsx';
 
+
+/**
+ * A component to hold the Field Info and Field Calendar components
+ */
+
 var Field = (props) => (
 
   <div className="main field-body">
