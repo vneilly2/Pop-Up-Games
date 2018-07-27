4bcf45939448c8f6a041b99882459cbb035b6cab
Sophia Lee <soph915@gmail.com>
2018-07-27 08:40:17 -0700

    add calendar to field page

--- client/src/components/field/FieldCalendar.jsx
+++ client/src/components/field/FieldCalendar.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import Calendar from 'react-calendar';
 
-var FieldCalendar = (props) => (
-  <div>
-  This will be a FieldCalendar
-  </div>
-  )
+class FieldCalendar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      date:''
+    }
+  }
+
+  onChange(date){
+    this.setState({ date })
+  }
+
+  render(){
+    return (
+      <div>
+        <Calendar
+          onChange={this.onChange}
+          value={this.state.date}
+        />
+      </div>
+    )
+  }
+
+}
 
 export default FieldCalendar;
--- client/src/components/field/FieldInfo.jsx
+++ client/src/components/field/FieldInfo.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 var FieldInfo = (props) => (
   <div>
       <h1>Field 1 Central Park </h1>
-      <div>Notes: has soccer nets permanently set up</div>
   </div>
   )
 
