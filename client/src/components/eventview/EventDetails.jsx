24ac4663f0462938a31c7c4224b382b58f3b3c5c
Max Berkowitz <maxl.berkowitz@gmail.com>
2018-07-30 10:23:43 -0700

    changed for 5:00 show

--- client/src/components/eventview/EventDetails.jsx
+++ client/src/components/eventview/EventDetails.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import { sportNumConv, blockToTime } from '../../../utils.js'
+import { sportNumConv, blockToTime } from '../../../utils.js';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 
 /**
  * @description component that distributes the various pieces of data about the event to sub-components
  */
-var EventDetails = (props) => {
-    return(
+var EventDetails = props => {
+  return (
     <div>
       <div className="event-entry-title">{props.details.eventName}</div>
       <div className="event-entry-date-time">{`Sport: ${sportNumConv(props.details.sportId)}`}</div>
       <div className="event-entry-date-time">
-        {
-        `${moment(props.details.date).format('LLL')} ${blockToTime(props.details.startBlock)} to ${blockToTime(props.details.endBlock)}`
-        }
+        {`${moment(props.details.date).format('LL')} ${blockToTime(props.details.startBlock)} to ${blockToTime(
+          props.details.endBlock
+        )}`}
       </div>
     </div>
-    )
-  }
+  );
+};
 
 EventDetails.propTypes = {
   details: PropTypes.object.isRequired,
-}
+};
 
 export default EventDetails;
